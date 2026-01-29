f0df792a00007622f1809184e841b6bc6145dc59
naveenaja <naveenajajala@gmail.com>
2026-01-29 06:39:57 +0000

    Fix tests using H2 and enable SonarCloud

--- Nebulytix_careerSecurity/src/test/java/com/neb/NebulytixBackendApplicationTests.java
+++ Nebulytix_careerSecurity/src/test/java/com/neb/NebulytixBackendApplicationTests.java
@@ -2,7 +2,10 @@ package com.neb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//added for testing purpose for the H2 data without mysql just for testing the application
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test") 
 @SpringBootTest
 class NebulytixBackendApplicationTests {
 
